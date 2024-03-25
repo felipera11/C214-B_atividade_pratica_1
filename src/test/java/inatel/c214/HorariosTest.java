7368b6f1a504f0cee4fdcef29275fdc61aa35225
felipera11 <felipe.s@ges.inatel.br>
2024-03-24 23:07:19 -0300

    created unit tests for Horario class

--- src/test/java/inatel/c214/HorariosTest.java
+++ src/test/java/inatel/c214/HorariosTest.java
@@ -1,7 +1,39 @@
 package inatel.c214;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
 
-public class HorariosTest extends TestCase {
+import org.junit.Test;
+
+public class HorariosTest{
+
+    @Test
+    public void testGetNomeDoProfessor(){
+        Horarios horario = new Horarios("Professor", "10:00", "Manhã", 1, 1);
+        assertEquals("Professor", horario.getNomeDoProfessor());
+    }
+
+    @Test
+    public void testGetHorarioDeAtendimento(){
+        Horarios horario = new Horarios("Professor", "10:00", "Manhã", 1, 1);
+        assertEquals("10:00", horario.getHorarioDeAtendimento());
+    }
+
+    @Test
+    public void testGetPeriodo(){
+        Horarios horario = new Horarios("Professor", "10:00", "Manhã", 1, 1);
+        assertEquals("Manhã", horario.getPeriodo());
+    }
+
+    @Test
+    public void testGetSala(){
+        Horarios horario = new Horarios("Professor", "10:00", "Manhã", 1, 1);
+        assertEquals(1, horario.getSala());
+    }
+
+    @Test
+    public void testGetPredio(){
+        Horarios horario = new Horarios("Professor", "10:00", "Manhã", 1, 1);
+        assertEquals(1, horario.getPredio());
+    }
 
 }
