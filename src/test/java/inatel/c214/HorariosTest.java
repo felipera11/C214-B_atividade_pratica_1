2ed0dd0394b59b05a8359d27a91ea79dcc17131d
felipera11 <felipe.s@ges.inatel.br>
2024-03-24 23:31:31 -0300

    Chris

--- src/test/java/inatel/c214/HorariosTest.java
+++ src/test/java/inatel/c214/HorariosTest.java
@@ -8,65 +8,65 @@ public class HorariosTest{
 
     @Test
     public void testGetNomeDoProfessor(){
-        Horarios horario = new Horarios("Professor", "10:00", "Manhã", 1, 1);
-        assertEquals("Professor", horario.getNomeDoProfessor());
+        Horarios horario = new Horarios("Chris", "10:00", "Manhã", 1, 1);
+        assertEquals("Chris", horario.getNomeDoProfessor());
     }
 
     @Test
     public void testGetHorarioDeAtendimento(){
-        Horarios horario = new Horarios("Professor", "10:00", "Manhã", 1, 1);
+        Horarios horario = new Horarios("Chris", "10:00", "Manhã", 1, 1);
         assertEquals("10:00", horario.getHorarioDeAtendimento());
     }
 
     @Test
     public void testGetPeriodo(){
-        Horarios horario = new Horarios("Professor", "10:00", "Manhã", 1, 1);
+        Horarios horario = new Horarios("Chris", "10:00", "Manhã", 1, 1);
         assertEquals("Manhã", horario.getPeriodo());
     }
 
     @Test
     public void testGetSala(){
-        Horarios horario = new Horarios("Professor", "10:00", "Manhã", 1, 1);
+        Horarios horario = new Horarios("Chris", "10:00", "Manhã", 1, 1);
         assertEquals(1, horario.getSala());
     }
 
     @Test
     public void testGetPredio(){
-        Horarios horario = new Horarios("Professor", "10:00", "Manhã", 1, 1);
+        Horarios horario = new Horarios("Chris", "10:00", "Manhã", 1, 1);
         assertEquals(1, horario.getPredio());
     }
 
     @Test
     public void testSetNomeDoProfessor(){
-        Horarios horario = new Horarios("Professor", "10:00", "Manhã", 1, 1);
-        horario.setNomeDoProfessor("Professor2");
-        assertEquals("Professor2", horario.getNomeDoProfessor());
+        Horarios horario = new Horarios("Chris", "10:00", "Manhã", 1, 1);
+        horario.setNomeDoProfessor("Renzo");
+        assertEquals("Renzo", horario.getNomeDoProfessor());
     }
 
     @Test
     public void testSetHorarioDeAtendimento(){
-        Horarios horario = new Horarios("Professor", "10:00", "Manhã", 1, 1);
+        Horarios horario = new Horarios("Chris", "10:00", "Manhã", 1, 1);
         horario.setHorarioDeAtendimento("11:00");
         assertEquals("11:00", horario.getHorarioDeAtendimento());
     }
 
     @Test
     public void testSetPeriodo(){
-        Horarios horario = new Horarios("Professor", "10:00", "Manhã", 1, 1);
+        Horarios horario = new Horarios("Chris", "10:00", "Manhã", 1, 1);
         horario.setPeriodo("Tarde");
         assertEquals("Tarde", horario.getPeriodo());
     }
 
     @Test
     public void testSetSala(){
-        Horarios horario = new Horarios("Professor", "10:00", "Manhã", 1, 1);
+        Horarios horario = new Horarios("Chris", "10:00", "Manhã", 1, 1);
         horario.setSala(2);
         assertEquals(2, horario.getSala());
     }
 
     @Test
     public void testSetPredio(){
-        Horarios horario = new Horarios("Professor", "10:00", "Manhã", 1, 1);
+        Horarios horario = new Horarios("Chris", "10:00", "Manhã", 1, 1);
         horario.setPredio(2);
         assertEquals(2, horario.getPredio());
     }
