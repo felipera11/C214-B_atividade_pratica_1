0e9aaca0818e377cfd519a460c9686841a56b446
felipera11 <felipe.s@ges.inatel.br>
2024-03-24 23:24:17 -0300

    created class RequestHorarios.java

---
+++ src/main/java/inatel/c214/RequestHorarios.java
@@ -0,0 +1,26 @@
+package inatel.c214;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class RequestHorarios {
+
+    public void requestHorarios(String json){
+        JsonObject jsonObject = JsonParser.parseString(json).getAsJsonObject();
+        String nomeDoProfessor = jsonObject.get("nomeDoProfessor").getAsString();
+        String horarioDeAtendimento = jsonObject.get("horarioDeAtendimento").getAsString();
+        String periodo = jsonObject.get("periodo").getAsString();
+        int sala = jsonObject.get("sala").getAsInt();
+        int predio = jsonObject.get("predio").getAsInt();
+        Horarios horario = new Horarios(nomeDoProfessor, horarioDeAtendimento, periodo, sala, predio);
+    }
+
+    public int calculatePredio(int sala){
+        if (sala <= 0) {
+            throw new IllegalArgumentException("Sala number must be greater than 0");
+        }
+        int indicePredio = (sala - 1) / 5;
+
+        return indicePredio;
+    }
+}
